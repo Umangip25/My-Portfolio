0539b8ba8a0c856d98b1c53820d6f11676820277
Umangi <umangi@Umangis-MacBook-Pro.local>
2026-06-02 15:49:42 -0700

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Syne, DM_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const syne = Syne({
   variable: "--font-syne",
@@ -66,6 +67,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
